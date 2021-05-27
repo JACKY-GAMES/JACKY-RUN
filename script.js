7789c21440d75ca9c77da4ed3ff51db3849a6aae
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2021-05-26 22:38:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,35 +13,7 @@ function jump() {
   setTimeout(() =>
     dino.classList.remove("jump-animation"), 500);
 }
-const startBtn = {
-    x : 120,
-    y : 263,
-    w : 83,
-    h : 29
-}
 
-// CONTROL THE GAME
-cvs.addEventListener("click", function(evt){
-    switch(state.current){
-        case state.getReady:
-            state.current = state.game;
-            SWOOSHING.play();
-            break;
-        case state.over:
-            let rect = cvs.getBoundingClientRect();
-            let clickX = evt.clientX - rect.left;
-            let clickY = evt.clientY - rect.top;
-            
-            // CHECK IF WE CLICK ON THE START BUTTON
-            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
-                rock.reset();
-                bird.speedReset();
-                score.reset();
-                state.current = state.getReady;
-            }
-            break;
-    }
-});
 document.addEventListener('click', (event) => {
   if (!dino.classList.contains('jump-animation')) {
 	  JUMPSOUND.play();
